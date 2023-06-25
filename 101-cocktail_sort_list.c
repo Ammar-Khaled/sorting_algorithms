f9177c1b11df02b17fa672a4a0c11fa870474415
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-06-25 15:49:13 +0000

    use malloc

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,6 +1,18 @@
 #include "sort.h"
 
 /**
+ * swap - swap two integers
+ * @a: the first integer
+ * @b: the second integer
+ */
+void swap(int *a, int *b)
+{
+	int temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/**
  * print_array - Prints an array of integers
  *
  * @array: The array to be printed
@@ -8,93 +20,73 @@
  */
 void print_array(const int *array, size_t size)
 {
-    size_t i;
+	size_t i;
 
-    i = 0;
-    while (array && i < size)
-    {
-        if (i > 0)
-            printf(", ");
-        printf("%d", array[i]);
-        ++i;
-    }
-    printf("\n");
+	i = 0;
+	while (array && i < size)
+	{
+		if (i > 0)
+			printf(", ");
+		printf("%d", array[i]);
+		++i;
+	}
+	printf("\n");
 }
 
 /**
- * cocktail_sort_list - sorts a doubly linked list of integers in ascending order
+ * cocktail_sort_list - sorts a doubly linked list of integers ascendingly
  * using the Cocktail shaker sort algorithm
  * @list: list of integers
  */
 void cocktail_sort_list(listint_t **list)
 {
-    int swapped, len, start, end, i, tmp;
-    listint_t *cur;
-    int arr[1000000];
+	int swapped, len, start, end, i;
+	listint_t *cur;
+	int *arr;
 
-    cur = *list;
-    len = 0;
-    while (cur)
-    {
-        len++;
-        cur = cur->next;
-    }
-
-
-    for (i = 0, cur = *list; i < len; i++, cur = cur->next)
-    {
-        arr[i] = cur->n;
-    }
-
-    swapped = 1;
-    start = 0;
-    end = len - 1;
-
-    while (swapped)
-    {
-        swapped = 0;
-
-        for (i = start; i < end; i++)
-        {
-            if (arr[i] > arr[i + 1])
-            {
-                tmp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = tmp;
-
-                swapped = 1;
-
-                print_array(arr, len);
-            }
-        }
-
-        if (!swapped)
-        {
-            break;
-        }
-
-        end--;
-
-        swapped = 0;
-
-        for (i = end - 1; i >= start; i--)
-        {
-            if (arr[i] > arr[i + 1])
-            {
-                tmp = arr[i];
-                arr[i] = arr[i + 1];
-                arr[i + 1] = tmp;
-
-                swapped = 1;
-
-                print_array(arr, len);
-            }
-        }
-        start++;
-    }
-
-    for (i = 0, cur = *list; i < len; i++, cur = cur->next)
-    {
-        cur->n = arr[i];
-    }
+	cur = *list;
+	len = 0;
+	while (cur)
+	{
+		len++;
+		cur = cur->next;
+	}
+	arr = malloc(sizeof(int) * len);
+	if (!arr)
+		exit(EXIT_FAILURE);
+	for (i = 0, cur = *list; i < len; i++, cur = cur->next)
+		arr[i] = cur->n;
+	swapped = 1;
+	start = 0;
+	end = len - 1;
+	while (swapped)
+	{
+		swapped = 0;
+		for (i = start; i < end; i++)
+		{
+			if (arr[i] > arr[i + 1])
+			{
+				swap(arr + i, arr + i + 1);
+				swapped = 1;
+				print_array(arr, len);
+			}
+		}
+		if (!swapped)
+			break;
+		end--;
+		swapped = 0;
+		for (i = end - 1; i >= start; i--)
+		{
+			if (arr[i] > arr[i + 1])
+			{
+				swap(arr + i, arr + i + 1);
+				swapped = 1;
+				print_array(arr, len);
+			}
+		}
+		start++;
+	}
+	for (i = 0, cur = *list; i < len; i++, cur = cur->next)
+		cur->n = arr[i];
+	free(arr);
 }
