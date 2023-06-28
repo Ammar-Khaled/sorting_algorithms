86e6e261928a3e3a67c45191a81e32790993e43c
Ammar-Khaled <ammar.khaled.github@gmail.com>
2023-06-28 02:53:12 +0000

    fix style

--- 106-bitonic_sort.c
+++ 106-bitonic_sort.c
@@ -8,86 +8,97 @@
  */
 void swap(int *a, int *b)
 {
-    int temp = *a;
-    *a = *b;
-    *b = temp;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
 
+/**
+ * bitonicMerge - bitonicMerge
+ * @arr: array
+ * @low: low index
+ * @cnt: count
+ * @dir: direction
+ * @size: size
+ */
 void bitonicMerge(int *arr, int low, int cnt, int dir, int size)
 {
-    int k, i;
-    char *direction;
+	int k, i;
+	char *direction;
 
-    if (dir == 1)
-    {
-        direction = "UP";
-    }
-    else
-    {
-        direction = "DOWN";
-    }
+	if (dir == 1)
+	{
+		direction = "UP";
+	}
+	else
+	{
+		direction = "DOWN";
+	}
 
-    if (cnt > 1)
-    {
-        k = cnt / 2;
-
-
-        for (i = 0; i < low + k; i++)
-        {
-            if (dir == (arr[i] > arr[i + k]))
-            {
-                swap(arr + i, arr + i + k);
-            }
-        }
-
-        bitonicMerge(arr, low, k, dir, size);
-        bitonicMerge(arr, low + k, k, dir, size);
+	if (cnt > 1)
+	{
+		k = cnt / 2;
 
 
-        printf("Result [%i/%i] (%s):\n", cnt, size, direction);
-        print_array(arr, cnt);
+		for (i = 0; i < low + k; i++)
+		{
+			if (dir == (arr[i] > arr[i + k]))
+			{
+				swap(arr + i, arr + i + k);
+			}
+		}
 
-        /*
-        if (strcmp(direction, "UP") == 0)
-        {
-            print_array(arr, k);
-        }
-        else
-        {
-            print_array(arr + k, k);
-        }
-        */
-    }
+		bitonicMerge(arr, low, k, dir, size);
+		bitonicMerge(arr, low + k, k, dir, size);
+
+
+		printf("Result [%i/%i] (%s):\n", cnt, size, direction);
+		print_array(arr, cnt);
+
+	}
 }
 
+/**
+ * bitonicSort - bitonicSort
+ * @arr: array
+ * @low: low index
+ * @cnt: count
+ * @dir: direction
+ * @size: size
+ */
 void bitonicSort(int *arr, int low, int cnt, int dir, int size)
 {
-    int k;
+	int k;
 
-    char *direction;
+	char *direction;
 
-    if (dir == 1)
-    {
-        direction = "UP";
-    }
-    else
-    {
-        direction = "DOWN";
-    }
+	if (dir == 1)
+	{
+		direction = "UP";
+	}
+	else
+	{
+		direction = "DOWN";
+	}
 
-    if (cnt > 1)
-    {
-        k = cnt / 2;
-        printf("Merging [%i/%i] (%s):\n", cnt, size, direction);
-        print_array(arr, cnt);
+	if (cnt > 1)
+	{
+		k = cnt / 2;
+		printf("Merging [%i/%i] (%s):\n", cnt, size, direction);
+		print_array(arr, cnt);
 
-        bitonicSort(arr, low, k, 1, size);
-        bitonicSort(arr, low + k, k, 0, size);
-        bitonicMerge(arr, low, cnt, dir, size);
-    }
+		bitonicSort(arr, low, k, 1, size);
+		bitonicSort(arr, low + k, k, 0, size);
+		bitonicMerge(arr, low, cnt, dir, size);
+	}
 }
 
+/**
+ * bitonic_sort - bitonic_sort
+ * @array: array
+ * @size: size
+ */
 void bitonic_sort(int *array, size_t size)
 {
-    bitonicSort(array, 0, size, 1, size);
+	bitonicSort(array, 0, size, 1, size);
 }
